5bd75d33870235c00a549947f5e44e6a8f2f4c22
alex <alex@alex.com>
2025-04-10 16:02:25 +0000

    cleanup;

--- test_rknn.py
+++ test_rknn.py
@@ -3,7 +3,5 @@ from ultralytics import YOLO
 # Load the exported RKNN model
 rknn_model = YOLO("yolo11n_rknn_model/yolo11n-rk3566.rknn", task='detect')
 
-rknn_model.info()
-
 # Run inference
 results = rknn_model("https://ultralytics.com/images/bus.jpg")
