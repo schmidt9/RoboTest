fddc8aa159fe4220300443c11336b58a6379abd4
Your Name <you@example.com>
2025-04-16 12:57:27 +0300

    remove sounddevice;

--- README.md
+++ README.md
@@ -5,10 +5,7 @@ Orange Pi 3B vision and speech recognition
 https://habr.com/ru/companies/timeweb/articles/772080/ - hardware setup
 https://habr.com/ru/companies/timeweb/articles/817929/ - software setup
 
-# setup recording
-
-- install sounddevice
-python -m pip install sounddevice
+sounddevice/pyaudio not working for some reason on Orange Pi 3B (Armbian), freezing on play, so using aplay command
 
 # helpers
 
--- speech/notifications.py
+++ speech/notifications.py
@@ -1,13 +1,15 @@
 import os
-import sounddevice
-import soundfile
+import subprocess
 
 def play_name_notification():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file_path = f'{dir_path}/sound/mixkit-correct-answer-tone-2870.wav'  # Извлечение данных и частоты дискретизации из файла
-    data, samplerate = soundfile.read(file_path, dtype='float32')
-    sounddevice.play(data, samplerate)
-    sounddevice.wait()
+
+    print("Start playing notification")
+
+    subprocess.run([f'aplay --device=plughw:0,0 {file_path}'], shell=True)
+
+    print("End playing notification")
 
 if __name__ == "__main__":
     play_name_notification()
