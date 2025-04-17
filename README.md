ecfa6318cabb02b96f564ffde33c03f3da3e08f0
Your Name <you@example.com>
2025-04-17 14:58:35 +0300

    add Piper

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 
 Orange Pi 3B vision and speech recognition
 
+# TTS setup
+
 https://habr.com/ru/companies/timeweb/articles/772080/ - hardware setup
 https://habr.com/ru/companies/timeweb/articles/817929/ - software setup
 
 pip install sounddevice
+
+# Piper
+
+Piper is faster then Silero
+
+https://github.com/rhasspy/piper/tree/master?tab=readme-ov-file#usage
+
+echo "Привет мир" | piper --model ru_RU-dmitri-medium.onnx --output_file test.wav 
+
+echo "Привет мир 16 50" | piper --model ru_RU-dmitri-medium.onnx --output-raw | aplay --device=plughw:0,0 -r 22050 -f S16_LE -t raw -
 
 # helpers
 
@@ -14,3 +26,5 @@ arecord -l
 
 - list playback devices
 aplay -l
+
+
--- speech/player.py
+++ speech/player.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
+import pytz
+import datetime
 import logger
+from pathlib import Path
 
 
 def play_file(file_path: str):
@@ -12,7 +15,27 @@ def play_file(file_path: str):
     logger.log("End playing file")
 
 
+def speak_text(text: str):
+    logger.log(f"Start speaking text '{text}'")
+
+    root_path = Path(__file__).parent
+    piper_path = f"{root_path}/piper"
+    
+    # not using sounddevice here because it freezes on play() for some reason
+
+    command = f"echo {text} | {piper_path}/piper --model {piper_path}/model/ru_RU-dmitri-medium.onnx --output-raw | aplay --device=plughw:0,0 -r 22050 -f S16_LE -t raw -"
+    logger.log(f"Executing command '{command}'")
+
+    subprocess.run([command], shell=True)
+
+    logger.log("End speaking text")
+
+
 if __name__ == "__main__":
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file_path = f"{dir_path}/sound/mixkit-correct-answer-tone-2870.wav"
     play_file(file_path)
+
+    timezone = pytz.timezone("Europe/Moscow")
+    now = datetime.datetime.now().astimezone(timezone)
+    speak_text(f"Текущее время по Москве {now.hour} {now.minute}")
