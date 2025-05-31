8c90230436b7b5602a0f640e6b93d89623c016e1
Your Name <you@example.com>
2025-05-31 13:34:44 +0000

    fix i2c master

--- keyestudio/i2c_master.py
+++ keyestudio/i2c_master.py
@@ -1,8 +1,18 @@
 # https://dronebotworkshop.com/i2c-arduino-raspberry-pi/
 
 from smbus3 import SMBus
- 
-addr = 0x3c # bus address
+
+addr = 0x8 # bus address
 bus = SMBus(2) # indicates /dev/i2c-2
 
-bus.write_byte(addr, 0x1)
+numb = 1
+
+print ("Enter command")
+
+while numb == 1:
+    command = input('>>>>   ').encode('utf-8')
+
+    bus.write_byte(addr, command[0])
+    byte_value = bus.read_byte(addr)
+    str_byte_value = byte_value.to_bytes(1, 'big').decode('utf-8')
+    print(f'Byte read: {byte_value} (string value: {str_byte_value})')
