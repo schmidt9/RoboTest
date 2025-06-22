75015b77d14cb01b07a5474b3367145fe4f244e2
Your Name <you@example.com>
2025-06-22 16:47:30 +0000

    fix paths

--- start_scripts.sh
+++ start_scripts.sh
@@ -4,7 +4,7 @@ REPO_ROOT=~/Documents/dev/RoboTest
 
 . $REPO_ROOT/venv/bin/activate
 
-python $REPO_ROOT/keyestudio/i2c_oled_ssd1306.py &
+python $REPO_ROOT/src/robotest/keyestudio/i2c_oled_ssd1306.py &
 
-python $REPO_ROOT/vision/yolo11_camera/webstreaming.py &
+python $REPO_ROOT/src/robotest/vision/yolo11_camera/webstreaming.py &
 
