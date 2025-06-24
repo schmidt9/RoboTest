e8a436f569d2e1cdda6bde0078b0ea5fbae2db0f
Your Name <you@example.com>
2025-06-24 21:12:43 +0000

    working on i2c;

--- src/robotest/keyestudio/i2c_master.py
+++ src/robotest/keyestudio/i2c_master.py
@@ -9,10 +9,17 @@ numb = 1
 
 print ("Enter command")
 
-while numb == 1:
-    command = input('>>>>   ').encode('utf-8')
 
-    bus.write_byte(addr, command[0])
+def send_command(command: int):
+    bus.write_byte(addr, command)
     byte_value = bus.read_byte(addr)
     str_byte_value = byte_value.to_bytes(1, 'big').decode('utf-8')
     print(f'Byte read: {byte_value} (string value: {str_byte_value})')
+
+
+while numb == 1:
+    command = input('>>>>   ').encode('utf-8')
+    send_command(command[0])
+
+
+
