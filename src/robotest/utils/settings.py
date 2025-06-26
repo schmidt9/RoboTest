ca81419eb9c21ab51d3b0813f6283460d751401e
ANK-PC\user <kormanowskij@yandex.ru>
2025-06-26 21:06:55 +0300

    fix read settings;

--- src/robotest/utils/settings.py
+++ src/robotest/utils/settings.py
@@ -1,16 +1,20 @@
 import json
+import os.path
+from pathlib import Path
+
+json_path = os.path.join(Path(__file__).parent, "settings.json")
 
 
 class Settings:
 
     def __init__(self) -> None:
-        json_obj = self.read()
-        self.speed_left: str = json_obj.speed.left
-        self.speed_right: str = json_obj.speed.left
-        self.speed_turn: str = json_obj.speed.turn
+        json_dict = self.read()
+        self.speed_left: str = json_dict["speed"]["left"]
+        self.speed_right: str = json_dict["speed"]["right"]
+        self.speed_turn: str = json_dict["speed"]["turn"]
 
     def read(self):
-        with open("settings.json") as file:
+        with open(json_path) as file:
             return json.load(file)
 
     def write(self):
@@ -18,8 +22,10 @@ class Settings:
             self, default=lambda o: o.__dict__, sort_keys=True, indent=4
         )
 
-        with open("settings.json") as file:
+        with open(json_path) as file:
             json.dump(json_str, file)
 
 
 settings = Settings()
+
+print(settings)
