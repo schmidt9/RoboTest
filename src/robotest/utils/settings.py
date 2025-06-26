f949f2abb1c7c6bee41b3f03bcca2819a2b14b4e
ANK-PC\user <kormanowskij@yandex.ru>
2025-06-27 00:14:05 +0300

    fix settings;

--- src/robotest/utils/settings.py
+++ src/robotest/utils/settings.py
@@ -1,28 +1,38 @@
 import json
 import os.path
 from pathlib import Path
+from dataclasses import dataclass, asdict
 
 json_path = os.path.join(Path(__file__).parent, "settings.json")
 
 
+@dataclass
 class Settings:
+    speed_left: int
+    speed_right: int
+    speed_turn: int
 
-    def __init__(self) -> None:
-        json_dict = self.read()
-        self.speed_left: int = json_dict["speed_left"]
-        self.speed_right: int = json_dict["speed_right"]
-        self.speed_turn: int = json_dict["speed_turn"]
-
-    def read(self):
+    @staticmethod
+    def read():
         with open(json_path) as file:
-            return json.load(file)
+            dict = json.load(file)
+            settings = Settings(**dict)
+            return settings
 
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+        dict = asdict(self)
+        return json.dumps(
+            dict,
+            ensure_ascii=False,
+            sort_keys=True,
+            indent=4,
+        )
 
     def write(self):
-        with open(json_path) as file:
-            json.dump(self.toJSON(), file)
+        dict = asdict(self)
+
+        with open(json_path, "w") as file:
+            json.dump(dict, file, sort_keys=True, ensure_ascii=False, indent=4)
 
 
-settings = Settings()
+settings = Settings.read()
