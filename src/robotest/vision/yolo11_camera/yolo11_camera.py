490cfb9ebde67144c4af5cd7e0e4d1b4dc1580a8
ANK-PC\user <kormanowskij@yandex.ru>
2025-06-26 21:39:10 +0300

    fix import

--- src/robotest/vision/yolo11_camera/yolo11_camera.py
+++ src/robotest/vision/yolo11_camera/yolo11_camera.py
@@ -4,7 +4,7 @@ import time
 import threading
 from pathlib import Path
 from robotest.vision.yolo11_camera.coco_utils import COCO_test_helper
-from rknn_executor import RKNN_model_container
+from robotest.vision.yolo11_camera.rknn_executor import RKNN_model_container
 import numpy as np
 import imutils
 
